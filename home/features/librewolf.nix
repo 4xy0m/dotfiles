8cb5c758657cc4bfbbcd320acf7a5228c36f63aa
Axyom <z3sifk0s@duck.com>
2024-11-27 00:12:48 +0100

    add extension to librewolf

--- home/features/librewolf.nix
+++ home/features/librewolf.nix
@@ -5,17 +5,17 @@
 }: {
   programs.librewolf = {
     enable = true;
-    #    profiles.default = {
-    #      extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-    #        ublock-origin
-    #        bitwarden
-    #        darkreader
-    # "2fas-two-factor-authentication"
-    # vimium
-    # stylus
-    # firefox-color
-    #      ];
-    #    };
+    profiles.default = {
+     extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+       ublock-origin
+       bitwarden
+       darkreader
+       # 2fas-two-factor-authentication
+       vimium
+       stylus
+       firefox-color
+     ];
+    };
     settings = {
       "privacy.clearOnShutdown.history" = false;
       "privacy.clearOnShutdown.downloads" = false;
