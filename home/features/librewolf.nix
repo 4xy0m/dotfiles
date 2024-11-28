659b931d551a4f9c27738355c15c9f358b6756bb
Axyom <z3sifk0s@duck.com>
2024-11-28 23:35:59 +0100

    Set brave search as default engine

--- home/features/librewolf.nix
+++ home/features/librewolf.nix
@@ -6,11 +6,28 @@
   programs.librewolf = {
     enable = true;
     profiles.default = {
+      search = {
+        force = true;
+        default = "Brave";
+        engines = {
+          "Brave" = {
+            urls = [
+              {
+                  template = "https://search.brave.com/search?q={searchTerms}";
+              }
+              {
+                  template = "https://search.brave.com/api/suggest?q={searchTerms}";
+                  type = "application/x-suggestions+json";
+              }
+            ];
+          };
+        };
+      };
      extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
        ublock-origin
        bitwarden
        darkreader
-       # 2fas-two-factor-authentication
+       inputs.firefox-addons.packages.${pkgs.system}."2fas-two-factor-authentication"
        vimium
        stylus
        firefox-color
