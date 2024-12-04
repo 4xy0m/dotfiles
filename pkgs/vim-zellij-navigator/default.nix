28c7cdc233af4759308e838dec92efa2952a04c0
Axyom <z3sifk0s@duck.com>
2024-12-04 06:49:27 +0100

    Removed vim-zellij-navitor package

--- pkgs/vim-zellij-navigator/default.nix
+++
@@ -1,5 +0,0 @@
-{fetchurl}:
-fetchurl {
-  url = "https://github.com/hiasr/vim-zellij-navigator/releases/download/0.2.1/vim-zellij-navigator.wasm";
-  sha256 = "sha256-wpIxPkmVpoAgOsdQKYuipSlDAbsD3/n6tTuOEriJHn0=";
-}
