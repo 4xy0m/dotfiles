789c6fe7d2007d95794b6bfc4d5a4055d4e65506
Axyom <z3sifk0s@duck.com>
2025-02-17 23:46:43 +0100

    add textfox theme to librewolf

--- home/features/librewolf.nix
+++ home/features/librewolf.nix
@@ -1,8 +1,17 @@
 {
   pkgs,
   inputs,
+  config,
   ...
-}: {
+}: 
+  let 
+    textfox = pkgs.fetchFromGitHub {
+      owner = "adriankarlen";
+      repo = "textfox";
+      rev = "e0d002eea7a5a3791bbf3f4b64c9197a531a4f96";
+      hash = "sha256-/g0f7j7qFUtO9tX7qMwVjIvb7LxRUjnm2c9wNU/kprQ=";
+    }; in 
+{
   programs.librewolf = {
     enable = true;
     profiles.default = {
@@ -36,6 +45,23 @@
     settings = {
       "privacy.clearOnShutdown.history" = false;
       "privacy.clearOnShutdown.downloads" = false;
+      "extensions.autoDisableScopes" = 0;
+      "extensions.enabledScopes" = 15;
     };
+  };
+  home.file = {
+    ".librewolf/default/chrome" = {
+      source = "${textfox}/chrome";
+      recursive = true;
+    };
+    ".librewolf/default/chrome/config.css".text = with config.colorScheme.colors; ''
+      :root { 
+        --tf-display-horizontal-tabs: block; 
+        --tf-bg: #${base00}; 
+        --tf-accent: #${base07}; 
+        --tf-border: #${base04}; 
+      }
+    '';
+    ".librewolf/default/user.js".source = "${textfox}/user.js";
   };
 }
