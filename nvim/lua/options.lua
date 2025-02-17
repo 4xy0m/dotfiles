a6a885f93549a3a8ebe85883578d497422940dbf
Axyom <z3sifk0s@duck.com>
2025-02-18 00:46:28 +0100

    allow nvim to communicate with yazi
    allow nvim to communicate with yazi in order to communicate the current
    file edited and focus on it

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,3 +1,13 @@
 require "nvchad.options"
 
 vim.wo.relativenumber = true
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    local filename = vim.api.nvim_buf_get_name(0)
+    local dirname = filename:match("(.*/)")
+    os.execute(string.format("ya emit-to $(expr $ZELLIJ_PANE_ID - 1) cd %s", dirname))
+    os.execute(string.format("ya emit-to $(expr $ZELLIJ_PANE_ID - 1) hove %s", filename))
+  end
+})
