d7d32ab44eacdfe059db0d4f2a802df34a52ec0f
Axyom <z3sifk0s@duck.com>
2025-02-18 02:05:58 +0100

    add some utility packages

--- home/bundles/cli.nix
+++ home/bundles/cli.nix
@@ -1,11 +1,14 @@
 {pkgs, ...}: {
   myHomeManager.zsh.enable = true;
   myHomeManager.starship.enable = true;
+  myHomeManager.cava.enable = true;
 
   home.packages = with pkgs; [
     ripgrep
     git
     gh
     fzf
+    playerctl
+    fastfetch
   ];
 }
