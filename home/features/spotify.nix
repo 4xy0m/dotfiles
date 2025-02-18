f67f139a8872eba274482b27af6bfd30558a9180
Axyom <z3sifk0s@duck.com>
2025-02-18 01:07:12 +0100

    add catppuccin theme

--- home/features/spotify.nix
+++ home/features/spotify.nix
@@ -2,10 +2,20 @@
   config,
   pkgs,
   ...
-}: {
+}: 
+  let 
+    catppuccin-spotify-player = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "spotify-player";
+      rev = "34b3d23806770185b72466d777853c73454b85a6";
+      hash = "sha256-eenf1jB8b2s2qeG7wAApGwkjJZWVNzQj/wEZMUgnn5U=";
+    };
+  in {
   programs.spotify-player = {
     enable = true;
+    settings.theme = "Catppuccin-mocha";
   };
+  xdg.configFile."spotify-player/theme.toml".source = "${catppuccin-spotify-player}/theme.toml";
   services.spotifyd = {
     enable = true;
     settings.global = {
