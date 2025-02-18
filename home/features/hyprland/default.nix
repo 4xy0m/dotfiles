3abed2b0344b87f10d2b9bae62c41bf0b0a9deae
Axyom <z3sifk0s@duck.com>
2025-02-18 02:05:37 +0100

    bind media key

--- home/features/hyprland/default.nix
+++ home/features/hyprland/default.nix
@@ -127,6 +127,15 @@
           "$mainMod, mouse:272, movewindow"
           "$mainMod, mouse:273, resizewindow"
         ];
+        bindel = [
+          ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioPrev, exec, playerctl previous"
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioNext, exec, playerctl stop"
+        ];
         workspace = map
           (
             m: lib.mkIf (m.rotating == 1) "m[${toString m.name}], layoutopt:orientation:top"
