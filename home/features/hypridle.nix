2a8b53fb4f272ce0f6fb68c212096cd972862247
Axyom <z3sifk0s@duck.com>
2026-03-17 08:06:30 +0100

    feat: check if the active window is fullscreen before idling

--- home/features/hypridle.nix
+++ home/features/hypridle.nix
@@ -10,16 +10,16 @@
       listener = [
         {
           timeout = 300;
-          on-timeout = "loginctl lock-session";
+          on-timeout = "sh -c 'hyprctl activewindow -j | jq -e \".fullscreen > 0\" >/dev/null || loginctl lock-session";
         }
         {
           timeout = 330;
-          on-timeout = "hyprctl dispatch dpms off";
+          on-timeout = "sh -c 'hyprctl activewindow -j | jq -e \".fullscreen > 0\" >/dev/null || hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
         {
           timeout = 600;
-          on-timeout = "systemctl suspend";
+          on-timeout = "sh -c 'hyprctl activewindow -j | jq -e \".fullscreen > 0\" >/dev/null || systemctl suspend";
         }
       ];
     };
